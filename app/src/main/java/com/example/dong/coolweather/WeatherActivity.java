2bf54232473662de62497a5758d51cf32fc0b5de
dong <dong@gmail.com>
2018-10-14 21:55:28 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/example/dong/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/dong/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.dong.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.dong.gson.Forecast;
 import com.example.dong.gson.Weather;
+import com.example.dong.service.AutoUpdateService;
 import com.example.dong.util.HttpUtil;
 import com.example.dong.util.Utility;
 
@@ -212,5 +214,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
